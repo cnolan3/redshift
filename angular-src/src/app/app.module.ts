c9b167f1979e4d41daed0844a80727fefe6e0502
Connor Nolan <connornolan15@gmail.com>
2018-02-25 08:12:58 -0800

    start initial front end design

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { InfoComponent } from './components/info/info.component';
+import { FooterComponent } from './components/footer/footer.component';
 
+
+const appRoutes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'info', component: InfoComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InfoComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
